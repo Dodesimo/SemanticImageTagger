5ef33092bae5ce1af6cedaec1ee85a89ee3e54d0
devam <devammondal06@gmail.com>
2024-12-28 19:00:00 -0500

    inserted into mongo db vector database

--- api/main.py
+++ api/main.py
@@ -1,13 +1,15 @@
 import os
+
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from PIL import Image
-import requests
 from langchain_openai import OpenAI, OpenAIEmbeddings
 from openai import OpenAI
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 app = FastAPI()
 load_dotenv()
+
 
 @app.get("/")
 def read_root():
@@ -16,7 +18,6 @@ def read_root():
 
 @app.get("/upload/{id}")
 def upload(id: str):
-
     client = OpenAI(api_key=os.getenv("KEY"))
 
     response = client.chat.completions.create(
@@ -25,12 +26,13 @@ def upload(id: str):
             {
                 "role": "user",
                 "content": [
-                    {"type": "text", "text": "What's in this image? Use cultural knowledge to provide a comprehensive three-sentence paragraph. Use proper nouns pertaining to people, but focus on describing the specific image."},
+                    {"type": "text",
+                     "text": "What's in this image? Use cultural knowledge to provide a comprehensive three-sentence paragraph. Use proper nouns pertaining to people, but focus on describing the specific image."},
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"https://sitproto.s3.us-east-2.amazonaws.com/{id}"
-,
+                            ,
                         },
                     },
                 ],
@@ -39,8 +41,16 @@ def upload(id: str):
 
         max_tokens=300,
     )
-
+    # Create embedding
     embeddings_model = OpenAIEmbeddings(model="text-embedding-3-large", api_key=os.getenv("KEY"))
-    return {"embedding": embeddings_model.embed_query(response.choices[0].message.content)}
+    e = embeddings_model.embed_query(response.choices[0].message.content)
 
+    uri = "mongodb+srv://dm:dm@cluster0.so2fb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db = client["photos"]
+    col = db["photos"]
+    entry = {"name": f"{id}", "embedding": e}
+    col.insert_one(entry)
 
+    return {"repsonse": 200}
